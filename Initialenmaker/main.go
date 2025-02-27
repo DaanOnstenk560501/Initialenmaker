1c978d149b0fd6f30a68086e6bec0047fad5aac4
Daan Onstenk 560501 <560501@student.fontys.nl>
2025-02-27 10:39:28 +0100

    Commit 9, Errors worden nu uitgevoerd door een methode en deze worden gelogd in de 'errors.log' file

--- Initialenmaker/main.go
+++ Initialenmaker/main.go
@@ -1,69 +1,82 @@
 package main
 
 import (
-        "bufio"
-        "fmt"
-        "os"
-        "regexp"
-        "strings"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strings"
 )
 
 func berekenInitialen(naam string) string {
-        tussenvoegsels := []string{"van", "den", "der", "de", "ten", "ter"}
-        woorden := strings.Split(strings.ToLower(naam), " ")
-        initialen := ""
+	tussenvoegsels := []string{"van", "den", "der", "de", "ten", "ter"}
+	woorden := strings.Split(strings.ToLower(naam), " ")
+	initialen := ""
 
-        for _, woord := range woorden {
-                isTussenvoegsel := false
-                for _, tv := range tussenvoegsels {
-                        if woord == tv {
-                                isTussenvoegsel = true
-                                break
-                        }
-                }
-                if !isTussenvoegsel {
-                        initialen += strings.ToUpper(string(woord[0])) + "."
-                }
-        }
+	for _, woord := range woorden {
+		isTussenvoegsel := false
+		for _, tv := range tussenvoegsels {
+			if woord == tv {
+				isTussenvoegsel = true
+				break
+			}
+		}
+		if !isTussenvoegsel {
+			initialen += strings.ToUpper(string(woord[0])) + "."
+		}
+	}
 
-        if len(initialen) > 0 {
-                return initialen[:len(initialen)-1]
-        }
-        return initialen
+	if len(initialen) > 0 {
+		return initialen[:len(initialen)-1]
+	}
+	return initialen
+}
+
+func error(message string, logMessage string) {
+	fmt.Println("Fout:", message)
+	log.Println(logMessage)
 }
 
 func main() {
-        reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 
-        for {
-                fmt.Print("Voer een naam in: ")
-                naam, _ := reader.ReadString('\n')
-                naam = strings.TrimSpace(naam)
+	logFile, err := os.OpenFile("errors.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
 
-                // Verwijder meerdere spaties
-                reSpaties := regexp.MustCompile(`\s+`)
-                naam = reSpaties.ReplaceAllString(naam, " ")
+	log.SetOutput(logFile)
 
-                if naam == "" {
-                        fmt.Println("Fout: Voer een naam in.")
-                } else if regexp.MustCompile(`\d`).MatchString(naam) {
-                        fmt.Println("Fout: Een naam mag geen cijfers bevatten.")
-                } else {
-                        // Controleer op ongeldige tekens (alles behalve letters en spaties)
-                        reOngeldig := regexp.MustCompile(`[^a-zA-Z\s]`)
-                        if reOngeldig.MatchString(naam) {
-                                fmt.Println("Fout: De naam bevat ongeldige tekens. Indien uw naam een apostrof of ander teken bevat hoeft u deze niet in te vullen")
-                        } else {
-                                fmt.Println(berekenInitialen(naam))
-                        }
-                }
+	for {
+		fmt.Print("Voer een naam in: ")
+		naam, _ := reader.ReadString('\n')
+		naam = strings.TrimSpace(naam)
 
-                fmt.Print("Type 'opnieuw' om opnieuw te proberen, of op Enter om af te sluiten: ")
-                input, _ := reader.ReadString('\n')
-                input = strings.TrimSpace(input)
+		reSpaties := regexp.MustCompile(`\s+`)
+		naam = reSpaties.ReplaceAllString(naam, " ")
 
-                if strings.ToLower(input) != "opnieuw" {
-                        break
-                }
-        }
+		if naam == "" {
+			error("Voer een naam in.", "Lege naam ingevoerd.")
+		} else if regexp.MustCompile(`\d`).MatchString(naam) {
+			error("Een naam mag geen cijfers bevatten.", fmt.Sprintf("Naam bevat cijfers: %s", naam))
+		} else {
+
+			reOngeldig := regexp.MustCompile(`[^a-zA-Z\s]`)
+			if reOngeldig.MatchString(naam) {
+				error("De naam bevat ongeldige tekens. Als de naam een apostrof bevat hoeft u deze niet in te vullen", fmt.Sprintf("Naam bevat ongeldige tekens: %s", naam))
+			} else {
+				fmt.Println(berekenInitialen(naam))
+			}
+		}
+
+		fmt.Print("Type 'opnieuw' om opnieuw te proberen, of op Enter om af te sluiten: ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		if strings.ToLower(input) != "opnieuw" {
+			break
+		}
+	}
 }
